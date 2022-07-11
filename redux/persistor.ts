c049b823f9370abb7fb090140451b33a67a101f0
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-11 18:20:22 +0430

    add cart slice

--- redux/persistor.ts
+++ redux/persistor.ts
@@ -4,19 +4,21 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./slices/userReducer";
 import savedItemsReducer from "./slices/savedItemsReducer";
+import cartReducer from "./slices/cartReducer";
 
 
 
 
 const rootReducer = combineReducers({
     user:userReducer,
-    savedItems:savedItemsReducer
+    savedItems:savedItemsReducer,
+    cart:cartReducer
 })
 
 const persistConfig = { 
     key:'root',
     storage,
-    blacklist:['user','savedItems']
+    blacklist:['user','savedItems','cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
---
+++ redux/slices/cartReducer.ts
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+interface Istate {
+    items:any[]
+}
+
+const initialState:Istate = {
+    items:[]
+}
+
+const cartSlice = createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        setCart:(state,action) => {
+            state.items = action.payload.items
+        },
+        addToCart:(state,action)=>{
+            const newItem = action.payload.item
+            const productIndex:number = state.items.findIndex((p)=>{
+                return p.product._id === newItem.product._id && p.size === newItem.size && p.color === newItem.color
+            })
+            if(productIndex>=0){
+                const newQuantity = state.items[productIndex].quantity + 1
+                state.items[productIndex].quantity = newQuantity
+            }else{
+                state.items = [...state.items, {...newItem,quantity:1}]
+            }
+        },
+        decreaseItemQuantity:(state,action)=>{
+            const {_id,color,size} = action.payload
+            const productIndex:number = state.items.findIndex((p)=>{
+                return p.product._id === _id && p.size === size && p.color === color
+            })
+            const newQuantity:number = state.items[productIndex].quantity - 1
+            if(newQuantity === 0){
+                state.items.splice(productIndex,1)
+            }else{
+                state.items[productIndex].quantity = newQuantity
+            }
+        }
+    }
+})
+
+export const {setCart, addToCart, decreaseItemQuantity} = cartSlice.actions
+
+export default cartSlice.reducer

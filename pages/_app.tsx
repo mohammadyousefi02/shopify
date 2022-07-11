835c521082aa4705c533b8f111070dfc75b9d348
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-11 17:28:15 +0430

    install and setup redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import {store,persistor} from "../redux/store"
+import { PersistGate } from 'redux-persist/integration/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
 
 export default MyApp

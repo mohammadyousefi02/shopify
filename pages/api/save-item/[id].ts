3ac72d38647c1e04bf244d441b19ca8d009cfd8d
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-11 17:52:25 +0430

    add save-item endpoint

---
+++ pages/api/save-item/[id].ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { savedItemHandler } from '../../../controllers/savedItemsController';
+
+import connectToDb from '../../../utils/coonectToDb';
+
+const saveItem = async(req:NextApiRequest,res:NextApiResponse)=>{
+
+    await connectToDb()
+    if(req.method === "POST") savedItemHandler(req,res,"save")
+    else res.status(400).send({error:"this is a bad request"})
+
+}
+
+
+
+export default saveItem

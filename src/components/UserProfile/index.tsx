7daf7f0e8a20591070efcc04d0d3699da06cea44
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-12 15:41:07 +0430

    add UserProfile component

---
+++ src/components/UserProfile/index.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import MainLayout from '../../layouts/MainLayout'
+import useAuthUserToken from "../../../hooks/useAuthUserToken"
+import Button from '../Button'
+import ListItem from '../ListItem'
+import {FaShippingFast, BiUserCircle} from "../../../icons"
+import { Iuser } from "../../../interfaces/userInterface"
+
+interface Props  {
+    user:Iuser
+}
+
+function UserProfile({user} : Props) {
+    const [token,setToken, removeToken] = useAuthUserToken()
+
+  return (
+    <MainLayout title='user profile'>
+        <div className='px-4 flex flex-col gap-4'>
+            <h1 className='text-center font-bold'>dear {user?.username}</h1>
+            <ListItem color='blue' title='profile info' Icon={BiUserCircle} href="/me" />
+            <ListItem color='green' title='orders' Icon={FaShippingFast} href="/orders-list"/>
+            <Button onClick={removeToken} title='log out' className='bg-red-500 w-full'/>
+        </div>
+    </MainLayout>
+  )
+}
+
+export default UserProfile

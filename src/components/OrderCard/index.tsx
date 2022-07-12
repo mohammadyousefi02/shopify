10c6948609676f1870d4e2f1da3e407cdb93ac83
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-12 15:38:40 +0430

    add order card component

---
+++ src/components/OrderCard/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+interface Iprops {
+    url:string,
+    title:string,
+    price:string,
+    quantity:number,
+    size:string,
+    color:string,
+}
+
+
+function OrderCard({url,title,price,quantity,size,color}:Iprops) {
+
+  return (
+    <div className='bg-white py-2 flex flex-col gap-2 justify-between shadow-md'>
+        <div className='relative h-56'>
+            <Image src={url} layout="fill" objectFit='contain' alt="aks"/>
+        </div>
+        <div className='px-2 text-center flex flex-col gap-1'>
+            <h1 className='font-semibold'>{title}</h1>       
+            <span className={`text-indigo-500 font-black`}>total: ${+(price) * quantity}</span> 
+            <span className={`text-indigo-500 font-black`}>size: {size}</span> 
+            <span className={`text-indigo-500 font-black`}>color: {color}</span> 
+        </div>
+        <h1 className='text-center'>quantity: {quantity}</h1>
+    </div>
+  )
+}
+
+
+export default OrderCard

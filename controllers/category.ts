f6d00f49d7f1ca9eef1a7c7ce219123c6a39a0f3
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-17 00:49:06 +0430

    SOY:feat(category):create category controller file

--- controllers/category.ts
+++ controllers/category.ts
@@ -21,6 +21,39 @@ const createCategory = async(req:NextApiRequest, res:NextApiResponse) => {
     }
 }
 
+const editCategory = async(req:NextApiRequest, res:NextApiResponse) => {
+    const token:string = <string>req.headers["x-auth-token"]
+    const decoded:IdecodedToken = <IdecodedToken>jwt.verify(token, process.env.jwtPrivateKey!);
+    const { id } = req.query
+    try {
+        if(decoded.isAdmin){
+            const {name} = req.body
+            let category = await Category.findByIdAndUpdate(id,{name},{new:true})
+            res.status(200).send(category)
+        }else{
+            res.status(403).send({error:'access denied'})
+        }
+    } catch (error) {
+        res.status(400).send({error})
+    }
+}
+
+const deleteCategory = async(req:NextApiRequest, res:NextApiResponse) => {
+    const token:string = <string>req.headers["x-auth-token"]
+    const decoded:IdecodedToken = <IdecodedToken>jwt.verify(token, process.env.jwtPrivateKey!);
+    const { id } = req.query
+    try {
+        if(decoded.isAdmin){
+            let category = await Category.findByIdAndDelete(id)
+            res.status(200).send(category)
+        }else{
+            res.status(403).send({error:'access denied'})
+        }
+    } catch (error) {
+        res.status(400).send({error})
+    }
+}
+
 const getAllCategories = async(req:NextApiRequest, res:NextApiResponse) => {
     try {
         const categories = await Category.find().populate('products')
@@ -40,4 +73,4 @@ const getOneCategory = async(req:NextApiRequest, res:NextApiResponse) => {
     }
 }
 
-export {createCategory, getAllCategories, getOneCategory}
+export {createCategory, getAllCategories, getOneCategory, editCategory, deleteCategory}

a5e8925cd10f955917ae5abda7b8ce0922d807d1
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-16 21:43:45 +0430

    edit(index):edit main page

--- src/components/ProductDetail/index.tsx
+++ src/components/ProductDetail/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { server } from '../../../config/server'
 import useAuthUserToken from '../../../hooks/useAuthUserToken'
 import Button from '../Button'
@@ -10,20 +10,28 @@ import { values } from 'lodash'
 import {useSelector, useDispatch} from "react-redux"
 import {setCart, addToCart} from "../../../redux/slices/cartReducer"
 import {v4 as uuidv4} from "uuid"
+import { Isize } from "../../../interfaces/productInterface"
 
 interface Iprops {
-    url:string,
+    images:string[],
     title:string,
     price:string,
     _id:string,
-    sizes:string[],
-    colors:string[]
+    sizes:Isize[],
 }
 
-function ProductDetail({url,title,price,sizes,colors,_id}:Iprops) {
+function ProductDetail({images,title,price,sizes,_id}:Iprops) {
     const dispatch = useDispatch()
 
+    interface Ioption {
+        label:string,
+        value:string
+    }
+
     const {items} = useSelector((store:any)=>store.cart)
+    const [sizeOptions, setSizeOptions] = useState<Ioption[]>([])
+    const [colorOptions, setColorOptions] = useState<Ioption[]>([])
+
     const router = useRouter()
     const [error,setError] = useState("")
 
@@ -31,19 +39,28 @@ function ProductDetail({url,title,price,sizes,colors,_id}:Iprops) {
     const [color,setColor] = useState("")
 
     const [token] = useAuthUserToken()
-    const sizeOptions = sizes.filter(s=>s!=="ALL").map(s=>{
-        return {value:s,label:s}
+   useEffect(()=>{
+    const copySizeOptions:Ioption[] = []
+    sizes.forEach(s=>{
+        copySizeOptions.push({value:s.size,label:s.size})
     })
+    setSizeOptions(copySizeOptions)
+   },[])
 
-    const colorOptions = colors.map(c=>{
-        return {value:c,label:c}
+   useEffect(()=>{
+    const chosenSize = sizes.find(s=>s.size === size)
+    const colorOptions:Ioption[] = []
+    chosenSize?.colors.forEach(c=>{
+        colorOptions.push({value:c,label:c})
     })
+    setColorOptions(colorOptions)
+   },[size])
 
     const addToCartHandler = async() => {
         if(token){
             if(size && color){
                 const copyCartItems = [...items]
-                dispatch(addToCart({item:{_id:uuidv4(),color,size,product:{image:url,name:title,price,_id}}}))
+                dispatch(addToCart({item:{_id:uuidv4(),color,size,product:{images,name:title,price,_id}}}))
                 try {
                     await axios.post(`${server}/api/add-to-cart/${_id}`,{size,color},{
                         headers:{
@@ -62,7 +79,7 @@ function ProductDetail({url,title,price,sizes,colors,_id}:Iprops) {
   return (
     <div className='bg-white pt-2 flex flex-col gap-2 justify-between shadow-md'>
         <div className='relative h-56'>
-            <Image src={url} layout="fill" objectFit='contain' alt="aks"/>
+            <Image src={images[0]} layout="fill" objectFit='contain' alt="aks"/>
         </div>
         <div className='px-2 text-center flex flex-col gap-1'>
             <h1 className='font-semibold'>{title}</h1>       

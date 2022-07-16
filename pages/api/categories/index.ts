de66a766387d9eaaaf447333f314e1774e95c1cd
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-17 00:20:15 +0430

    create category route with id

---
+++ pages/api/categories/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import connectToDb from "../../../utils/coonectToDb";
+
+import { createCategory, getAllCategories } from "../../../controllers/category";
+
+
+export default async function handler(req: NextApiRequest,res: NextApiResponse) {
+
+    await connectToDb()
+    if(req.method === "GET")getAllCategories(req,res)
+    else if(req.method === "POST")createCategory(req,res)
+    else res.status(400).send({error:"this is a bad request"})
+
+}

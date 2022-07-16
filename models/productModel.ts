d9c0991b325d49651c5267dc49ac522f28c6aa3a
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-17 00:38:04 +0430

    edit(productModel):add decrease quantity method

--- models/productModel.ts
+++ models/productModel.ts
@@ -1,3 +1,4 @@
+import { Isize } from './../interfaces/productInterface';
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
@@ -13,6 +14,13 @@ const productSchema = new mongoose.Schema({
     }],
     category:String
 })
+
+
+productSchema.methods.decreaseQuantity = function(size:string, quantity:number){
+    const index = this.sizes.findIndex((i:Isize)=>i.size === size)
+    this.sizes[index].quantity = this.sizes[index].quantity - quantity
+    return this.save()
+}
 
 const Products = mongoose.models.Products || mongoose.model("Products",productSchema)
 

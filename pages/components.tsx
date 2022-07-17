6d428614eb456faa1c13cf65c5527a0eaea93427
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-18 02:21:01 +0430

    SOY-editBtnComponent:(components):test Button

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Button from '../src/components/Button'
 import ProductCart from '../src/components/ProductCard'
 
 function Component() {
   return (
-    <div className='p-4 flex '>
+    <div className='p-4 flex flex-col gap-4'>
         <ProductCart/>
+        <Button title='test' color='blue'/>
+        <Button title='test' color='blue' outline={true}/>
+        <Button title='test' color='green'/>
+        <Button title='test' color="red"/>
+        <Button title='test' color='gray'/>
+        <Button title='test' rounded='normal'/>
     </div>
   )
 }

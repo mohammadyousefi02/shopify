875be8a14f1ba8815d166bd4a48429a939e36aa2
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-18 02:19:32 +0430

    SOY-editBtnComponent:edit(Button)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
+import classNames from "classnames"
+
 interface Iprops {
     title:string,
     className?:string,
-    onClick?:()=>void
+    onClick?:()=>void,
+    color?: 'blue' | 'green' | 'red' | 'gray',
+    outline?:boolean,
+    rounded?: 'normal' | 'large'
 }
 
-function MyButton({title,className="",onClick}:Iprops) {
+function Button({title,className="",color="blue",outline=false,rounded="large",onClick}:Iprops) {
+  const btnClasses = classNames(
+    `px-2 py-3 text-white transition duration-75`,
+    color === "blue" && !outline ? 'bg-[#1E73EE] hover:bg-[#1E73BE]':'',
+    color === "blue" && outline ? 'bg-transparent border border-[#1E73BE] text-[#1E73BE] hover:bg-[#1E73BE] hover:text-white': '',
+    color === "green" ? 'bg-[#56B261] hover:bg-[#1E7E34]' : '',
+    color === "red" ? 'bg-red-400 hover:bg-red-500' : '',
+    color === "gray" ? 'bg-gray-400 hover:bg-gray-500' : '',
+    rounded === "large" ? 'rounded-[24px]' : 'rounded-[12px]', 
+    className
+  )
   return (
-    <button onClick={onClick} className={`px-2 py-3 rounded text-white ${className}`}>{title}</button>
+    <button onClick={onClick} className={btnClasses}>{title}</button>
   )
 }
 
-export default MyButton
+export default Button

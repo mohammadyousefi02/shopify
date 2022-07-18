0d3badd67cd0998053fe06a843a3a94507458ce6
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-19 01:46:29 +0430

    SOY-createHeader:feat(Badge):create Badge Componen

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import classNames from "classnames"
+
+interface Props {
+    number:number,
+    className?:string
+}
+
+function Badge({number,className}:Props) {
+   const badgeClasses = classNames(
+        'absolute bottom-0 right-[-4px] text-[10px] pt-1 w-[12px] h-[14px] bg-[#1E73BE] flex items-center justify-center rounded-full text-white',
+        className
+    )
+  return (
+    <span className={badgeClasses}>{number}</span>
+  )
+}
+
+export default Badge

3273cbd928dc8c2b085465a392a6aa9dd6b4e2dd
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-20 02:20:35 +0430

    SOY-createCategoryCard:edit(CategoryCard)

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 function CategoryCard({title, products}:Props) {
   return (
-    <div>
-        <h1>{title}</h1>
-        <div>
-        <div className='grid grid-cols-1 px-4 md:grid-cols-4 gap-4'>
-              {products?.map((p:Iproduct)=>(
-                  <ProductCart images={p.images} price={p.price} title={p.name} _id={p._id} key={p._id}/>
-              ))}
+    <div className='w-full flex flex-col gap-2'>
+        <h1 className="text-4xl text-[#1E73BE]">{title}</h1>
+        <div className='flex items-center w-full'>
+            <div className='grid grid-cols-1 px-4 w-full md:grid-cols-4 gap-4'>
+                {products?.map((p:Iproduct)=>(
+                    <ProductCart images={p.images} price={p.price} title={p.name} code={p.number} _id={p._id} key={p._id}/>
+                ))}
             </div>
         </div>
     </div>

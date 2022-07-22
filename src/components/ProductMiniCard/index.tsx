7a507662c5d9d0d70616373c8be000ba9ab9d6a6
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-22 21:12:37 +0430

    SOY-editDetails:edit(ProductMiniCard):change price color to primary

--- src/components/ProductMiniCard/index.tsx
+++ src/components/ProductMiniCard/index.tsx
@@ -15,7 +15,7 @@ function ProductMiniCard({image, title, price, className=""}:Props) {
             <Image src={image} width={75} height={75} alt={title} className="rounded-xl"/>
             <h1 className='text-[18px]'>{title}</h1>
         </div>
-        <h1 className='text-[#56B261]'>{price} تومان</h1>
+        <h1 className='text-primary'>{price} تومان</h1>
     </div>
   )
 }

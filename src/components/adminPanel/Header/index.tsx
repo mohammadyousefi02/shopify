419c64780c49167f35194876723c7c4737a0f544
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-26 13:58:23 +0430

    SOY-admin:edit(admin/Header):change links

--- src/components/adminPanel/Header/index.tsx
+++ src/components/adminPanel/Header/index.tsx
@@ -1,10 +1,17 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import useAuthUserToken from '../../../../hooks/useAuthUserToken'
 import Button from '../../Button'
 import Li from '../../Li'
 
 function Header() {
+    const router = useRouter()
     const [token, setToken, deleteToken] = useAuthUserToken()
+    const headerLinks = [
+        {title:'دسته بندی', link:'/admin/categories'},
+        {title:'محصولات', link:'/admin/products'},
+        {title:'سفارش ها', link:'/admin/orders'},
+    ]
   return (
     <div className='bg-white pt-2'>
         <div className='container mx-auto px-4'>
@@ -14,9 +21,9 @@ function Header() {
                     <Button onClick={deleteToken} title='خروج' color='pink' rounded='normal' className='w-16'/>
                 </div>
                 <div className='flex items-center gap-4 pt-4 pb-2'>
-                    <Li className='text-primary'>دسته بندی ها</Li>
-                    <Li>محصولات</Li>
-                    <Li>سفارش ها</Li>
+                    {headerLinks.map(({title, link}) => (
+                        <Li key={title} className={router.pathname === link ? "text-primary" : ''} href={link}>{title}</Li>
+                    ))}
                 </div>
             </div>
         </div>

abffcb1400fc4fcb08b8c63d9a02669c7308c1bc
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-28 02:23:53 +0430

    SOY-pagination:feat(Table):add pagination

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { MdDelete, MdEdit } from "../../../icons"
+import { useDispatch, useSelector } from 'react-redux'
+import usePagination from '../../../hooks/usePagination'
+import {setPage} from "../../../redux/slices/pagination";
 
 interface Props {
     th:string[],
@@ -10,8 +13,19 @@ interface Props {
 }
 
 function Table({th, tbody, onEdit, onDelete}:Props) {
+    const dispatch = useDispatch()
+    const { totalPage, page } = useSelector((state:any) => state.pagination)
+    const data = usePagination(tbody.data, 5, page)
+    const paginationButtons = []
+    for (let i = 1; i <= totalPage; i++) {
+        const button = (
+            <span onClick={()=>dispatch(setPage(i))} className={`bg-primary cursor-pointer pt-1 px-2 flex items-center justify-center rounded text-white ${page === i ? "bg-[#cc2b3e]" : ""}`}>{i}</span>
+        )
+        paginationButtons.push(button)
+    }
   return (
-    <table className='w-full'>
+    <div>
+        <table className='w-full'>
         <thead className="border-b border-gray">
             <tr>
                 {th.map(t=>(
@@ -20,8 +34,8 @@ function Table({th, tbody, onEdit, onDelete}:Props) {
             </tr>
         </thead>
         <tbody>
-            {tbody.data.map(d=>(
-                <tr key={d[tbody.by]}>
+            {data.map(d=>(
+                <tr key={d._id}>
                     <td>{d[tbody.by]}</td>
                     <td>
                         <div className='flex gap-1 text-base'>
@@ -36,7 +50,11 @@ function Table({th, tbody, onEdit, onDelete}:Props) {
             </tr>
             ))}
         </tbody>
-    </table>
+        </table>
+        <div className='flex justify-center items-center gap-1 mt-4 py-2'>
+              {[paginationButtons]}
+        </div>
+    </div>
   )
 }
 

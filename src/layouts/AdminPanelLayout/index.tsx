91b5c494202f674bf265b1e0fc75198ab90bc6da
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-28 02:22:33 +0430

    SOY-pagination:import and call convertNumToPer

--- src/layouts/AdminPanelLayout/index.tsx
+++ src/layouts/AdminPanelLayout/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../components/adminPanel/Header'
+import convertNumToPer from '../../../utils/convertNumToPer'
+
 
 interface Props {
     children?:React.ReactNode
 }
 
 function AdminPanelLayout({children}:Props) {
+  convertNumToPer()
   return (
     <div>
         <Header/>

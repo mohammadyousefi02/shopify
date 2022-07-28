62bb0531527ec56722024fab6d68b0a29a942a50
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-29 03:04:01 +0430

    SOY-workOnProject:feat(productReducer):add filter by price action for search and categories

--- redux/slices/productsReducer.ts
+++ redux/slices/productsReducer.ts
@@ -11,7 +11,12 @@ interface Istate {
     filterByCategoryValue:string,
     searchedProducts:Iproduct[],
     filteredProducts:Iproduct[],
-    productsByCategory:Iproduct[]
+    productsByCategory:Iproduct[],
+    filteredCategoryProducts:Iproduct[],
+    categoryMinPrice:string,
+    categoryMaxPrice:string,
+    searchMinPrice:string,
+    searchMaxPrice:string,
 }
 
 const initialState:Istate = {
@@ -19,10 +24,15 @@ const initialState:Istate = {
     filterByNameValue:"",
     filteredProducts:[],
     productsByCategory:[],
+    filteredCategoryProducts:[],
     searchedProducts:[],
     sort:'جدید ترین',
     categorySort:'جدید ترین',
-    filterByCategoryValue:"همه کالاها"
+    filterByCategoryValue:"همه کالاها",
+    categoryMinPrice:"0",
+    categoryMaxPrice:"500000",
+    searchMinPrice:"0",
+    searchMaxPrice:"500000",
 }
 
 const productsSlice = createSlice({
@@ -48,6 +58,8 @@ const productsSlice = createSlice({
             if(!state.filterByNameValue)state.filteredProducts = []
             else{
                 state.sort = 'جدید ترین'
+                state.searchMinPrice = "0"
+                state.searchMaxPrice = "500000"
                 state.filterByCategoryValue = "همه کالاها"
                 state.filteredProducts = []
                 state.products.map(p=>{
@@ -59,22 +71,36 @@ const productsSlice = createSlice({
         },
         filterSearchedProducts:(state)=>{
             !(state.filterByCategoryValue === "همه کالاها") ? state.filteredProducts = state.searchedProducts.filter(p=>p.category === state.filterByCategoryValue) :  state.filteredProducts = state.searchedProducts
-            state.filteredProducts = _.orderBy(state.filteredProducts,[state.sort === 'جدید ترین' ? "postedAt" : 'price'],[state.sort === 'ارزان ترین' ? 'asc' : 'desc']);
+            state.filteredProducts = state.filteredProducts.filter(p=>_.inRange(Number(p.price),Number(state.searchMinPrice), Number(state.searchMaxPrice)))
+            state.filteredProducts = _.orderBy(state.filteredProducts,[state.sort === 'جدید ترین' ? "postedAt" : 'price'],[state.sort === 'ارزان ترین'? 'asc' : 'desc']);
         },
         setProductsByCategory:(state,action)=>{
             const products:Iproduct[] = []
             state.categorySort = 'جدید ترین'
+            state.categoryMinPrice = "0"
+            state.categoryMaxPrice = "500000"
+            
             state.products.forEach(p=>{
                 if(p.category === action.payload)products.push(p)
             })
             state.productsByCategory = _.orderBy(products, ['postedAt'], ['desc']);
+            state.filteredCategoryProducts = state.productsByCategory
+        },
+        changeCategoryPriceRange:(state, action) => {
+            if(action.payload.min) state.categoryMinPrice = action.payload.min
+            if(action.payload.max) state.categoryMaxPrice = action.payload.max
+        },
+        changesearchPriceRange:(state, action) => {
+            if(action.payload.min) state.searchMinPrice = action.payload.min
+            if(action.payload.max) state.searchMaxPrice = action.payload.max
         },
         sortProductsByCategory:(state) => {
+            state.productsByCategory = state.filteredCategoryProducts.filter(p=>_.inRange(Number(p.price),Number(state.categoryMinPrice), Number(state.categoryMaxPrice)))
             state.productsByCategory = _.orderBy(state.productsByCategory,[state.categorySort === 'جدید ترین' ? "postedAt" : 'price'],[state.categorySort === 'ارزان ترین' ? 'asc' : 'desc']);
         }
     }
 })
 
-export const { setProducts, setFilterByNameValue, setProductsBySearch, filterSearchedProducts, setProductsByCategory, changeSortValue, changeCategorySortValue, changeFilterByCategoryValue, sortProductsByCategory} = productsSlice.actions
+export const { setProducts, setFilterByNameValue, setProductsBySearch, filterSearchedProducts, setProductsByCategory, changeSortValue, changeCategorySortValue, changeFilterByCategoryValue, sortProductsByCategory, changeCategoryPriceRange, changesearchPriceRange} = productsSlice.actions
 
 export default productsSlice.reducer

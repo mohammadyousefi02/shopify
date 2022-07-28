b863baa70be290fd778991cd7e323e109c6e3ca0
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-28 10:57:36 +0430

    SOY-pagination:feat(Table):use usePagination

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -15,14 +15,7 @@ interface Props {
 function Table({th, tbody, onEdit, onDelete}:Props) {
     const dispatch = useDispatch()
     const { totalPage, page } = useSelector((state:any) => state.pagination)
-    const data = usePagination(tbody.data, 5, page)
-    const paginationButtons = []
-    for (let i = 1; i <= totalPage; i++) {
-        const button = (
-            <span onClick={()=>dispatch(setPage(i))} className={`bg-primary cursor-pointer pt-1 px-2 flex items-center justify-center rounded text-white ${page === i ? "bg-[#cc2b3e]" : ""}`}>{i}</span>
-        )
-        paginationButtons.push(button)
-    }
+    const [data, paginationButtons] = usePagination(tbody.data, 5, page)
   return (
     <div>
         <table className='w-full'>
@@ -52,7 +45,7 @@ function Table({th, tbody, onEdit, onDelete}:Props) {
         </tbody>
         </table>
         <div className='flex justify-center items-center gap-1 mt-4 py-2'>
-              {[paginationButtons]}
+              {paginationButtons}
         </div>
     </div>
   )

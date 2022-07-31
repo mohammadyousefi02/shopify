16ac93eb38be40096d19a643af910b9cfa95ee2c
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-31 06:33:57 +0430

    SOY-singleProduct:feat(api/products/index):create products/id endpoint

---
+++ pages/api/products/[id]/index.tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { deleteProduct, editProduct, getOneProduct } from '../../../../controllers/product';
+import connectToDb from '../../../../utils/coonectToDb';
+
+export default async function getProductById( req: NextApiRequest,res: NextApiResponse ){
+
+        await connectToDb()
+        if(req.method === "GET") getOneProduct(req,res)
+        else if(req.method === "PUT") editProduct(req,res)
+        else if(req.method === "DELETE") deleteProduct(req,res)
+        else res.status(400).send({error:"this is a bad request"})
+        
+}

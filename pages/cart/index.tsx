c2384dc6d0fe0b15d53b6710b2f2701939114ea1
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-01 17:07:53 +0430

    SOY-cart:feat(cart):add EmptyCart and CartSection component

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,37 +1,21 @@
-import Link from 'next/link'
-import React, { useState } from 'react'
-import useGetUserId from '../../hooks/useGetUserId'
-import { Iuser } from '../../interfaces/userInterface'
-import CartCard from '../../src/components/CartCard'
-import Button from '../../src/components/Button'
-import MainLayout from '../../src/layouts/MainLayout'
-import { useSelector } from "react-redux"
-import { IcartItem } from '../../interfaces/cartInterface'
-
-interface Props{
-  user:Iuser
-}
+import React from "react";
+import MainLayout from "../../src/layouts/MainLayout";
+import { useSelector } from "react-redux";
+import EmptyCart from "../../src/components/cart/EmptyCart";
+import CartSection from "../../src/components/cart/CartSection";
 
 function Cart() {
+  const { items } = useSelector((store: any) => store.cart);
 
-
-  const {items} = useSelector((store:any)=>store.cart)
   return (
     <>
-        <MainLayout>
-        <div className='px-4 flex flex-col gap-4 pb-[90px]'>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-            {items?.map((p:IcartItem)=>(
-              <CartCard images={p.product.images} price={p.product.price} key={p._id} title={p.product.name} color={p.color} size={p.size} _id={p.product._id} quantity={p.quantity}/>
-            ))}
-          </div>
-              <Link href={'/payment'}>
-                <Button title='order' className='bg-red-500'/>
-              </Link>
-          </div>
-        </MainLayout>
+      <MainLayout>
+        <div className="container mx-auto p-4">
+          {!items.length ? <EmptyCart /> : <CartSection />}
+        </div>
+      </MainLayout>
     </>
-  )
+  );
 }
 
-export default Cart
+export default Cart;

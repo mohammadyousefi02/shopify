3e6444091248d373c436345658d15327ef8bd4f7
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-24 17:40:38 +0430

    SOY-admin:feat(categoryModal):removeProduct method added

--- models/categoryModel.ts
+++ models/categoryModel.ts
@@ -10,7 +10,20 @@ const categorySchema = new mongoose.Schema({
 })
 
 categorySchema.methods.addProduct = function(id:mongoose.Schema.Types.ObjectId){
-    this.products = [...this.products, id]
+    const products = this.products
+    const index = products.indexOf(id)
+    if(index === -1){
+        this.products = [...this.products, id]
+    }
+    return this.save()
+}
+
+categorySchema.methods.removeProduct = function(id:mongoose.Schema.Types.ObjectId){
+    const products = this.products
+    const index = products.indexOf(id)
+    if(index !== -1){
+        this.products = products.filter((i:mongoose.Schema.Types.ObjectId)=>i !== id)
+    }
     return this.save()
 }
 

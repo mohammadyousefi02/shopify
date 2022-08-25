47d0a716fdfcba2daddf1e9be70c0e5ae8007c18
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-25 12:23:48 +0430

    SOY-final:feat([...slug]):getStar function created

--- pages/products/[...slug].tsx
+++ pages/products/[...slug].tsx
@@ -10,28 +10,42 @@ import {useSelector, useDispatch} from "react-redux"
 import { setProductsByCategory } from '../../redux/slices/productsReducer'
 import _ from 'lodash'
 import Comments from '../../src/components/SingleProduct/Comments'
+import { useRouter } from 'next/router'
 
 interface Props {
     product:Iproduct
 }
 
 function SingleProduct({product}:Props) {
+    const router = useRouter()
     const [relativeProducts, setRelativeProducts] = useState<Iproduct[]>([])
     const dispatch = useDispatch()
     const {productsByCategory,products} = useSelector((store:any)=>store.products)
+    const [star, setStar] = useState("0")
+
+    const getStar = async() => {
+        const res = await axios.get(`${server}/api/products/${product._id}/stars`)
+        setStar(res.data)
+    }
+
     useEffect(()=>{
         dispatch(setProductsByCategory(product.category))
     },[products])
+
     useEffect(()=>{
         const relativeProducts:Iproduct[] = _.take(productsByCategory, 6)
         setRelativeProducts(relativeProducts)
     },[productsByCategory])
+
+    useEffect(()=>{
+        getStar()
+    },[router.query.slug])
   return (
     <MainLayout>
         <div className='container mx-auto px-4 mt-9 pb-[90px]'>
-            <ProductDetail _id={product._id} star={product.star} code={product.number} category={product.category} price={product.price} title={product.name} sizes={product.sizes} images={product.images}/>
+            <ProductDetail _id={product._id} star={star} code={product.number} category={product.category} price={product.price} title={product.name} sizes={product.sizes} images={product.images}/>
             <RelativeProduct products={relativeProducts}/>
-            <Comments id={product._id} />
+            <Comments id={product._id} getStar={getStar}/>
         </div>
     </MainLayout>
   )
